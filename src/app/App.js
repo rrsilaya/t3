5ce030fee91a91aa8458cc3a71ab117d137eb35b
rrsilaya <rrsilaya@up.edu.ph>
2018-05-01 18:54:27 +0800

    Add particles to background

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
 import {
   Board
 } from '../features';
 
+import config from './particles';
 import styles from './styles.css';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.appWrapper}>
+        <Particles params={config} className={styles.background}/>
         <Board/>
       </div>
     );

3ea1016bf343e9937dcb1ea9fb2cb045d4eb2efb
kajeagentspi <jptagnepis@up.edu.ph>
2018-05-04 00:14:11 +0800

    Updated working ai

--- src/features/ai/ai.js
+++ src/features/ai/ai.js
@@ -1,3 +1,4 @@
+let choice;
 var readline = require('readline-sync');
 const checkWinner = (piece, state) => {
   const winnerX = [
@@ -29,52 +30,58 @@ const checkWinner = (piece, state) => {
     .map(winner => state.match(winner))
     .some(element => !!element);
   const draw = state.match(drawRegex);
-  if (winX) return piece === 'X' ? 1 : -1;
-  else if (winO) return piece === 'Y' ? 1 : -1;
+  if (winX) return piece === 'X' ? -1 : 1;
+  else if (winO) return piece === 'Y' ? -1 : 1;
   else if (draw) return 0;
   else return 'None';
 };
 
-const getNextStates = (piece, state) => {
-  const nextMoves = state.split('').reduce((acc, current, index) => {
+const getAvailableMoves = (piece, state) => {
+  return state.split('').reduce((acc, current, index) => {
     if (current === ' ') acc.push(index);
     return acc;
   }, []);
-  return nextMoves.map(move => {
-    const nextState = state.split('');
-    nextState[move] = piece;
-    return nextState.join('');
-  });
 };
 
 const AICaller = (piece, state) => {
-  const nextStates = getNextStates(piece, state);
-  console.log(nextStates);
-  const result = aimove(0, piece, state);
-  console.log(result);
-  return nextStates[result];
+  const result = aimove(piece, state);
+  const currentState = state.split('');
+  currentState[choice] = piece;
+  const nextState = currentState.join('');
+  return nextState;
 };
 
-function aimove(level, piece, state) {
+function aimove(piece, state) {
   const nextPiece = piece === 'X' ? 'O' : 'X';
   const score = checkWinner(nextPiece, state);
-  if (score === 'None') {
-    const moves = getNextStates(piece, state).map(nextState => {
-      return aimove(level + 1, nextPiece, nextState);
-    });
-    let bestMove;
-    if (piece === 'X') {
-      bestMove = moves.indexOf(Math.min(...moves));
-    } else {
-      bestMove = moves.indexOf(Math.max(...moves));
-    }
-    return bestMove;
-  } else return score;
+  if (score !== 'None') {
+    return score;
+  }
+  const availableMoves = getAvailableMoves(piece, state);
+  const moves = [];
+  const scores = [];
+  for (let i = 0; i < availableMoves.length; i++) {
+    const move = availableMoves[i];
+    const currentState = state.split('');
+    currentState[move] = piece;
+    const nextState = currentState.join('');
+    scores.push(aimove(nextPiece, nextState));
+    moves.push(move);
+  }
+  if (piece === 'O') {
+    const index = scores.indexOf(Math.max(...scores));
+    choice = moves[index];
+    return scores[index];
+  } else {
+    const index = scores.indexOf(Math.min(...scores));
+    choice = moves[index];
+    return scores[index];
+  }
 }
-
-let piece;
-let lastpiece;
+let piece = '';
 let state = '         ';
+// AICaller(piece, state);
+let lastpiece;
 let status;
 for (let i = 0; i < 9; i++) {
   piece = i % 2 === 0 ? 'X' : 'O';
@@ -89,14 +96,21 @@ for (let i = 0; i < 9; i++) {
   if (i % 2 !== 0) {
     state = AICaller(piece, state);
   } else {
-    let move = parseInt(readline.question('Move: '), 10);
+    let move = parseInt(readline.question('Move: '), 10) - 1;
     state = state.split('');
     state[move] = piece;
     state = state.join('');
   }
   status = checkWinner(piece, state);
   if (status != 'None') {
-    console.log(state);
+    console.log(
+      `
+      =====
+      |${state.slice(0, 3)}|
+      |${state.slice(3, 6)}|
+      |${state.slice(6, 9)}|
+      =====`
+    );
     break;
   }
   lastpiece = piece;

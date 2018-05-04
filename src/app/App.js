477b3375c0b0de48ea080d850ce3dbe201387324
rrsilaya <rrsilaya@up.edu.ph>
2018-05-04 11:15:29 +0800

    Integrate to AI

--- src/app/App.js
+++ src/app/App.js
@@ -2,64 +2,101 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import { CSSTransition } from 'react-transition-group';
 
-import {
-  Board,
-  Message
-} from '../features';
+import { AICaller, checkWinner } from '../features/ai/ai';
+
+import { Board, Message } from '../features';
 
 import config from './particles';
 import styles from './styles.css';
+
+const Player = {
+  user: 'O',
+  ai: 'X'
+};
+
+const PLAYER = 1,
+  AI = -1,
+  DRAW = 0,
+  INGAME = 'None';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       board: Array.from({ length: 9 }, () => ' ').join(''),
-      turn: 'X',
+      winner: 'None',
+    };
+  }
 
-      isMessageOpen: false
+  handleRandomTurn = () => {
+    if (Math.round(Math.random())) {
+      this.setState({ board: AICaller(Player.ai, this.state.board) });
     }
+  }
+
+  handleGameReset = () => {
+    let board = Array.from({ length: 9 }, () => ' ').join('');
+
+    if (Math.round(Math.random())) {
+      board = AICaller(Player.ai, board);
+    }
+
+    this.setState({ board, winner: INGAME });
   }
 
   handleBoardTurn = index => {
     const { turn } = this.state;
-    const board = [...this.state.board];
+    let board = [...this.state.board];
     index = parseFloat(index);
 
     if (board[index] === ' ') {
-      board[parseFloat(index)] = this.state.turn;
-      this.setState({
-        board: board.join(''),
-        turn: turn === 'X' ? 'O' : 'X'
-      });
+      board[parseFloat(index)] = Player.user;
+
+      let winner = checkWinner(Player.ai, board.join(''));
+
+      if (winner !== PLAYER) {
+        board = AICaller(Player.ai, board.join(''));
+        winner = checkWinner(Player.ai, board);
+      }
+
+      this.setState({ board, winner });
     }
-  }
+  };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isMessageOpen: true })
-    }, 2000);
+    this.handleRandomTurn();
+    // setTimeout(() => {
+    //   this.setState({ isMessageOpen: true })
+    // }, 2000);
   }
 
   render() {
-    const { board } = this.state;
+    const { board, winner } = this.state;
 
     return (
       <div className={styles.appWrapper}>
-        <Particles params={config} className={styles.background}/>
+        <Particles params={config} className={styles.background} />
         <CSSTransition
-          in={this.state.isMessageOpen}
+          in={winner !== INGAME}
           timeout={1000}
-          classNames="message"
+          classNames={{
+            enter: styles.messageEnter,
+            enterActive: styles.messageEnterActive
+          }}
           unmountOnExit
+          appear={true}
           onEntered={() => {
             setTimeout(() => {
               this.setState({ isMessageOpen: false });
-            }, 1500)
-          }}
-        >
-          <Message message="Hello" />
+            }, 5000);
+          }}>
+          <Message
+            message={
+              winner === PLAYER ? 'You won!' : winner === AI ? 'You lose :(.' : "It's a draw."
+            }
+          />
         </CSSTransition>
+        <button onClick={this.handleGameReset}>Restart Game</button>
         <Board board={board} handleBoardTurn={this.handleBoardTurn} />
       </div>
     );
--- src/features/ai/ai.js
+++ src/features/ai/ai.js
@@ -1,5 +1,6 @@
-var readline = require('readline-sync');
-const checkWinner = (piece, state) => {
+// const readline = require('readline-sync');
+
+export const checkWinner = (piece, state) => {
   const winnerX = [
     /XXX....../,
     /...XXX.../,
@@ -28,7 +29,9 @@ const checkWinner = (piece, state) => {
   const winO = winnerO
     .map(winner => state.match(winner))
     .some(element => !!element);
+
   const draw = state.match(drawRegex);
+
   if (winX) return piece === 'X' ? -1 : 1;
   else if (winO) return piece === 'Y' ? -1 : 1;
   else if (draw) return 0;
@@ -42,27 +45,29 @@ const getAvailableMoves = (piece, state) => {
   }, []);
 };
 
-const AICaller = (piece, state) => {
-  const result = aimove(piece, state);
-  const { boardIndex } = result;
+export const AICaller = (piece, state) => {
+  const { boardIndex } = aimove(piece, state);
+
   const currentState = state.split('');
   currentState[boardIndex] = piece;
-  const nextState = currentState.join('');
-  return nextState;
+
+  return currentState.join('');
 };
 
 function aimove(piece, state) {
   const nextPiece = piece === 'X' ? 'O' : 'X';
   const score = checkWinner(nextPiece, state);
+
   if (score !== 'None') {
     return { score };
   }
+
   const availableMoves = getAvailableMoves(piece, state);
   const boardStates = availableMoves.map(boardIndex => {
     const currentState = state.split('');
     currentState[boardIndex] = piece;
-    const nextState = currentState.join('');
-    const score = aimove(nextPiece, nextState).score;
+
+    const { score } = aimove(nextPiece, currentState.join(''));
     return { score, boardIndex };
   });
 
@@ -70,6 +75,7 @@ function aimove(piece, state) {
     prev.push(curr.score);
     return prev;
   }, []);
+
   let index;
   if (piece === 'O') {
     index = scores.indexOf(Math.max(...scores));
@@ -78,40 +84,41 @@ function aimove(piece, state) {
   }
   return boardStates[index];
 }
-let piece = '';
-let state = '         ';
-// AICaller(piece, state);
-let lastpiece;
-let status;
-for (let i = 0; i < 9; i++) {
-  piece = i % 2 === 0 ? 'X' : 'O';
-  console.log(
-    `${piece}\n
-    =====
-    |${state.slice(0, 3)}|
-    |${state.slice(3, 6)}|
-    |${state.slice(6, 9)}|
-    =====`
-  );
-  if (i % 2 !== 0) {
-    state = AICaller(piece, state);
-  } else {
-    let move = parseInt(readline.question('Move: '), 10);
-    state = state.split('');
-    state[move] = piece;
-    state = state.join('');
-  }
-  status = checkWinner(piece, state);
-  if (status != 'None') {
-    console.log(
-      `
-      =====
-      |${state.slice(0, 3)}|
-      |${state.slice(3, 6)}|
-      |${state.slice(6, 9)}|
-      =====`
-    );
-    break;
-  }
-  lastpiece = piece;
-}
+
+// let piece = '';
+// let state = '         ';
+// // AICaller(piece, state);
+// let lastpiece;
+// let status;
+// for (let i = 0; i < 9; i++) {
+//   piece = i % 2 === 0 ? 'X' : 'O';
+//   console.log(
+//     `${piece}\n
+//     =====
+//     |${state.slice(0, 3)}|
+//     |${state.slice(3, 6)}|
+//     |${state.slice(6, 9)}|
+//     =====`
+//   );
+//   if (i % 2 !== 0) {
+//     state = AICaller(piece, state);
+//   } else {
+//     let move = parseInt(readline.question('Move: '), 10);
+//     state = state.split('');
+//     state[move] = piece;
+//     state = state.join('');
+//   }
+//   status = checkWinner(piece, state);
+//   if (status != 'None') {
+//     console.log(
+//       `
+//       =====
+//       |${state.slice(0, 3)}|
+//       |${state.slice(3, 6)}|
+//       |${state.slice(6, 9)}|
+//       =====`
+//     );
+//     break;
+//   }
+//   lastpiece = piece;
+// }
